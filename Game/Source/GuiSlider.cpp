96546dc0ce101cabbf72f0536f3a2a1be572ccf2
Makinilla-maker <valverdeoriol@gmail.com>
2020-12-26 15:59:15 +0100

    Slider more or less implemented

--- Game/Source/GuiSlider.cpp
+++ Game/Source/GuiSlider.cpp
@@ -30,18 +30,22 @@ bool GuiSlider::Update(Input* input, float dt)
             {
 
                 state = GuiControlState::PRESSED;
-                if (bounds.x > 300)
+
+                if (bounds.x + bounds.w >= 170)
                 {
-                    bounds.x = 300;
+                    bounds.x = mouseX - (bounds.w / 2);  // Slider
+                    if (bounds.x + bounds.w <= 170 + 130)
+                    {
+                        bounds.x = mouseX - (bounds.w / 2);  // Slider
+                    }
+                    else
+                    {
+                        bounds.x = 170 + 130 - bounds.w;
+                    }
                 }
-                else if (bounds.x < 170)
+                else
                 {
-                    bounds.x = 170;
-                }
-                if (bounds.x >= 170 && bounds.x <= 300)
-                {
-                    bounds.x = mouseX - (bounds.w / 2);
-                    NotifyObserver();
+                    bounds.x = 172;
                 }
                 
 
